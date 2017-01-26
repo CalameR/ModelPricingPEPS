d11d0734b6c4f91b24eb9cfc8e14a455817583ea
Raphaël CALAME <raphael.calame@ensimag.grenoble-inp.fr>
2017-01-26 18:51:11 +0100

    perf

--- src/test_PerformanceOption.cpp
+++ src/test_PerformanceOption.cpp
@@ -34,22 +34,32 @@ int main(int argc, char **argv)
     BlackScholesModel *bSM4 = new BlackScholesModel(size4,r4,trends,dividends,sigma4,spot4,rho);
 
     double fdStep4 = 0.01;
-    int nbSamples4 = 500000;
+    int nbSamples4 = 1;
 
     MonteCarloPricer *MC4 = new MonteCarloPricer(bSM4, Opt4, fdStep4, nbSamples4);
     double prix4;
     double ic4;
 
-    MC4->price(NULL,0,prix4,ic4,false);
+    /*
+    MC4->price(NULL,0,prix4,ic4,true);
 
     std::cout << "prix = " << prix4 << "\n";
 
     assert((prix4 - ic4 < 1.257353) && (prix4 + ic4 > 1.257353));
     cout << "4rd MonteCarlo Initial Price OK" << "\n";
+    */
+
+    time_t before;
+    time_t after;
+    double computingTime;
 
 
     std::cout << "Simulation de la couverture : " << " \n" ;
-    SimulationHedger::hedging_PL_Prices(MC4,Opt4->nbTimeSteps*60,"PerfPrices.txt","PortfolioPricesPerf.txt","timePerf.txt",!true);
+    time(&before);
+    SimulationHedger::hedging_PL_Prices(MC4,Opt4->nbTimeSteps*2,"PerfPrices.txt","PortfolioPricesPerf.txt","timePerf.txt",true);
+    time(&after);
+    computingTime = difftime(after,before);
+    std::cout << "Temps de calcul = " << computingTime << " secondes";
 
     pnl_vect_free(&lambda4);
     pnl_vect_free(&sigma4);
