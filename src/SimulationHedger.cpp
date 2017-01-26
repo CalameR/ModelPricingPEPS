e5df8d86467bcd59f49110a3fcae46ee4dbc603e
Raphaël CALAME <raphael.calame@ensimag.grenoble-inp.fr>
2017-01-26 10:48:14 +0100

    script only prices actigo

--- src/SimulationHedger.cpp
+++ src/SimulationHedger.cpp
@@ -187,4 +187,93 @@ double SimulationHedger::hedging_PL(MonteCarloPricer *monteCarloPricer, int H, b
     return freePortfolioValue + riskPortfolioValue - priceProduct;
 }
 
+void SimulationHedger::prices(MonteCarloPricer *monteCarloPricer, int H, char* fileProductName, char* fileTimeName, bool isParallel) {
+
+    if (H <= 0) {
+        throw std::invalid_argument("Number of discretization steps must be positive!");
+    }
+
+    if ((H % monteCarloPricer->prod->nbTimeSteps) != 0) {
+        throw std::invalid_argument("Number of discretization steps must be a multiple of time steps!");
+    }
+
+    PnlMat *path = pnl_mat_create(H + 1, monteCarloPricer->prod->nbAssets);
+    PnlMat *pathPayoff = pnl_mat_create(1, monteCarloPricer->prod->nbAssets);
+
+    PnlVect *assetsPrices = pnl_vect_create(monteCarloPricer->prod->nbAssets);
+
+    double currentDate = 0;
+    double timeToNextDate = (monteCarloPricer->prod->maturity)/((double)H);
+
+    double price;
+    double ic;
+
+    ofstream fileProduct(fileProductName,ios::out|ios::trunc);
+    ofstream fileTime(fileTimeName,ios::out|ios::trunc);
+
+    if (fileProduct && fileTime) {
+
+        monteCarloPricer->mod->simulateUnderHistoricalProba(path,monteCarloPricer->prod->maturity,0, H,(*monteCarloPricer->poolRng)(),NULL);
+
+        if (monteCarloPricer->prod->getName() == "Actigo") {
+
+            ofstream fileESX50("ESX50.txt",ios::out|ios::trunc);
+            ofstream fileSSP500("SSP500.txt",ios::out|ios::trunc);
+            ofstream fileSSP200("SSP200.txt",ios::out|ios::trunc);
+            ofstream fileXED("XED.txt",ios::out|ios::trunc);
+            ofstream fileXEA("XEA.txt",ios::out|ios::trunc);
+
+            double esx50_0 = MGET(path, 0, 0);
+            double ssp500_0 = MGET(path, 0, 1);
+            double ssp200_0 = MGET(path, 0, 2);
+            double xed_0 = MGET(path, 0, 3);
+            double xea_0 = MGET(path, 0, 4);
+            for (int i=0; i <= H; i++) {
+                fileESX50 << MGET(path,i,0)/esx50_0 << "\n";
+                fileSSP500 << MGET(path,i,1)/ssp500_0 << "\n";
+                fileSSP200 << MGET(path,i,2)/ssp200_0 << "\n";
+                fileXED << MGET(path,i,3)/xed_0 << "\n";
+                fileXEA << MGET(path,i,4)/xea_0 << "\n";
+            }
+            fileESX50.close();
+            fileSSP500.close();
+            fileSSP200.close();
+            fileXEA.close();
+            fileXED.close();
+
+        }
+
+
+        for (int i = 0; i <= H; i++) {
+            pnl_mat_get_row(assetsPrices, path, i);
+
+            if (i == 0 || (i - 1) % (H / monteCarloPricer->prod->nbTimeSteps) != 0) {
+                pnl_mat_set_row(pathPayoff, assetsPrices, pathPayoff->m - 1);
+            } else {
+                pnl_mat_add_row(pathPayoff, pathPayoff->m, assetsPrices);
+            }
+            monteCarloPricer->price(pathPayoff,currentDate,price,ic,isParallel);
+            fileProduct << price << "\n";
+            currentDate += timeToNextDate;
+        }
+
+        for (int i=0; i <= H; i++) {
+            double t = ((double)i)*(monteCarloPricer->prod->maturity)/(double)H;
+            fileTime << t << "\n";
+        }
+
+        fileProduct.close();
+        fileTime.close();
+
+        std::cout << "Prix du produit à échéance = " << price << " €\n";
+
+        std::cout << "\nPour visualiser les cours des actifs et celui du produit, lancer les scripts R " << " \n";
+    }
+
+    pnl_mat_free(&pathPayoff);
+    pnl_vect_free(&assetsPrices);
+    pnl_mat_free(&path);
+
+}
+
 
